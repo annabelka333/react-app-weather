e7ac9e7c73c523b86bd613d2598fbdd96638d853
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-01-23 23:06:33 +0200

    created open-source link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <p>
+        This project is created by{" "}
+        <a href="https://www.instagram.com/vediashkyna/" target="_blank">
+          Anna Vediashkyna
+        </a>{" "}
+        and is
+        {""}
+        <a
+          href="https://github.com/annabelka333/react-app-weather"
+          target="_blank"
+        >
+          {""} open-source on GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
